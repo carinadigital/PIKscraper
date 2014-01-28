0bbeb8219ecfeb1f7078d44d9ed78a404c1bae41
carinadigital <git@carinadigital.com>
2014-01-28 14:07:42 +0000

    Write an rss feed containing the video files.

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
-require_once 'vendor/fabpot/goutte/goutte.phar';
-
+require_once 'vendor/autoload.php';
 use Goutte\Client;
+use Suin\RSSWriter\Feed;
+use Suin\RSSWriter\Channel;
+use Suin\RSSWriter\Item;
+
 
 $client = new Client();
 // Go to Petrino Potami category page.
@@ -12,5 +15,32 @@ $crawler = $client->request('GET', 'http://www.cybc-media.com/video/index.php/vi
 $video_links = $crawler->filter('urlset > url > video > content_loc')->each(function ($node) {
   return $node->text();
 });
+
+
+// Setup a feed with the new data.
+$feed = new Feed();
+$channel = new Channel();
+$channel
+    ->title("Petrino Potami Feed")
+    ->description("Channel Description")
+    ->url('http://blog.example.com')
+    ->appendTo($feed);
+
+// Add links to the feed.
+foreach ($video_links as $video_link) {
+  // Parse links to ensure they are valid.
+  $url_parts = parse_url($video_link);
+  // Split the path to find the filename.
+  $filename = end(explode('/', $url_parts['path']));
+
+  $item = new Item();
+  $item
+    ->title($filename)
+    ->description("$filename videofile.")
+    ->url($video_link)
+    ->appendTo($channel);
+}
+
+echo $feed;
 
 ?>
