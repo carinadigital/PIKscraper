0d0950e540533164c7cfc87270852d066ce45795
carinadigital <git@carinadigital.com>
2014-01-28 12:57:58 +0000

    Return the video links in an array.

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ $client = new Client();
 // Go to Petrino Potami category page.
 $crawler = $client->request('GET', 'http://www.cybc-media.com/video/index.php/video-on-demand?task=view&cat=64&view=xml');
 
-// Get the latest post in this category and display the titles.
-$crawler->filter('urlset > url > video > content_loc')->each(function ($node) {
-   print $node->text()."\n";
+// Get the latest video links in this category.
+$video_links = $crawler->filter('urlset > url > video > content_loc')->each(function ($node) {
+  return $node->text();
 });
 
 ?>
